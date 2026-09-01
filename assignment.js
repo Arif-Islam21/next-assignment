191615632fab7ddfbea78b42acb6d34ef70f5dc4
Arif-Islam21 <arif.sojib2020@gmail.com>
2026-09-01 23:14:25 +0600

    question one done

---
+++ assignment.js
@@ -0,0 +1,26 @@
+// Question 1
+const describeValue = (value) => {
+  const type = typeof value;
+  const truthyOrFalsy = value ? "truthy" : "falsy";
+  return `"${type} | ${truthyOrFalsy}"`;
+};
+
+// Question 2
+const getDayType = (day) => {
+  const dayLowerCase = day.toLowerCase();
+  const days = [
+    "Friday",
+    "Saturday",
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+  ];
+  const daysLowerCase = days.map((day) => day.toLowerCase());
+  if (typeof day != "string" || !daysLowerCase.includes(dayLowerCase)) {
+    return "Invalid Day";
+  }
+};
+
+console.log(getDayType("Tuesdayy"));
